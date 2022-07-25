a26a8a43d06a6dee04bf3b52abba4dcce64fbbf4
박세연 <wsx4379@naver.com>
2022-07-25 22:59:10 +0900

    브루트 포스

---
+++ 10 - 브루트 포스/bj_2789.py
@@ -0,0 +1,14 @@
+# 블랙잭
+
+n, m = map(int, input().split())
+num = list(map(int, input().split()))
+
+result = 0
+for i in range(n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            card = num[i] + num[j] + num[k]
+            if card <= m and m - card < m - result:
+                result = card
+
+print(result)

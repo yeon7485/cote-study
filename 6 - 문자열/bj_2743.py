5f46bfd83381f1220963d34d29a871edb2e2ce10
박세연 <wsx4379@naver.com>
2023-04-03 19:38:49 +0900

    23.04.03

---
+++ 6 - 문자열/bj_2743.py
@@ -0,0 +1,5 @@
+# 단어 길이 재기
+# https://www.acmicpc.net/problem/2743
+
+s = input()
+print(len(s))
---
+++ 6 - 문자열/bj_27866.py
@@ -0,0 +1,6 @@
+# 문자와 문자열
+# https://www.acmicpc.net/problem/27866
+
+s = input()
+i = int(input())
+print(s[i-1])

0d3aa6899731313160a20ca07049da76a6036dab
박세연 <wsx4379@naver.com>
2023-04-07 19:10:50 +0900

    23.04.07

---
+++ 심화1/bj_2444.py
@@ -0,0 +1,11 @@
+# 별 찍기 - 7
+# https://www.acmicpc.net/problem/2444
+
+n = int(input())
+
+for i in range(1, n):
+    print(' ' * (n-i) + '*' * (2*i-1))
+
+
+for i in range(n, 0, -1):
+    print(' ' * (n-i) + '*' * (2*i-1))

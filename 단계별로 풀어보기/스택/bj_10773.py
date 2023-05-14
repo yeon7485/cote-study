b45ee5c5515641f269fd2321115fac1053c968e3
박세연 <wsx4379@naver.com>
2023-05-15 03:47:56 +0900

    23.05.15

---
+++ 단계별로 풀어보기/스택/bj_10773.py
@@ -0,0 +1,16 @@
+# 제로
+
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+
+for i in range(n):
+    n = int(sys.stdin.readline())
+    if n == 0:
+        if len(stack) > 0:
+            stack.pop()
+    else:
+        stack.append(n)
+
+print(sum(stack))

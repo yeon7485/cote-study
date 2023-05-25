ccebedd518a2583ae0a476b5a9251029bf84a49b
박세연 <wsx4379@naver.com>
2023-05-25 23:19:57 +0900

    23.05.25

---
+++ 단계별로 풀어보기/DFS/bj_11724.py
@@ -0,0 +1,33 @@
+# 연결 요소의 개수
+
+import sys
+sys.setrecursionlimit(10000)
+
+def dfs(v):
+    visited[v] = True
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(n+1)]
+
+visited = [True] + [False] * n
+
+for i in range(m):
+    x, y = map(int, sys.stdin.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+result = 1
+i = 1
+while True:
+    dfs(i)
+    if False not in visited:
+        break
+    result += 1
+    i = visited.index(False)
+
+print(result)

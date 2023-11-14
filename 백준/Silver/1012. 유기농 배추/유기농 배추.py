1c571a98e365bd3d89c0dc41de6ae3b3ff0a4f6e
박세연 <55083958+yeon7485@users.noreply.github.com>
2023-11-14 23:26:19 +0900

    [Silver II] Title: 유기농 배추, Time: 356 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+def bfs(x, y):
+    queue.append((x, y))
+    while queue:
+        x, y = queue.popleft()
+        visited[x][y] = True
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+            if graph[nx][ny] == 1 and not visited[nx][ny]:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+    return True
+
+
+T = int(input())
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+for t in range(T):
+    m, n, k = list(map(int, input().split()))
+    graph = [[0 for _ in range(m)] for __ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    queue = deque()
+    point = []
+    result = 0
+
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[b][a] = 1
+        point.append((b, a))
+
+    for p in point:
+        a, b = p
+        if not visited[a][b] and graph[a][b] == 1:
+            bfs(a, b)
+            result += 1
+
+    print(result)

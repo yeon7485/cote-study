006d5d3019a62d3221042b0617ab04f6b69fa7c0
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-02-29 22:58:55 +0900

    [Silver III] Title: N과 M (4), Time: 60 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,18 @@
+n, m = list(map(int, input().split()))
+visited = [False] * (n + 1)
+arr = []
+
+
+def dfs(count):
+    if count == m:
+        print(*arr, end='\n')
+        return
+
+    for i in range(1, n + 1):
+        if count == 0 or i >= arr[-1]:
+            arr.append(i)
+            dfs(count + 1)
+            arr.pop()
+
+
+dfs(0)

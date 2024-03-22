b811976c02179aa13a448a3a4daa6bd0a7ff1c9e
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-03-23 02:58:11 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -2,7 +2,7 @@ N, K = list(map(int, input().split()))
 coins = [int(input()) for _ in range(N)]
 rst = 0
 for i in range(N-1, -1, -1):
-    rst += int(K / coins[i])
+    rst += K // coins[i]
     K %= coins[i]
 
 print(rst)

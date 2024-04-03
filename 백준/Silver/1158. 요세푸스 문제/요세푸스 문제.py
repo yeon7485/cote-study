09992e6e4319594cb40d328f05da615e39f788d9
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-04-04 04:45:52 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 6016 ms, Memory: 34000 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+n, k = map(int, input().split())
+queue = deque()
+result = []
+
+for i in range(1, n + 1):
+    queue.append(i)
+
+x = 0
+while len(result) < n:
+    num = queue.popleft()
+    x += 1
+    if x == k:
+        result.append(num)
+        x = 0
+    else:
+        queue.append(num)
+
+ans = '<'
+for r in result:
+    ans += str(r) + ', '
+
+print(ans[:-2] + '>')
+

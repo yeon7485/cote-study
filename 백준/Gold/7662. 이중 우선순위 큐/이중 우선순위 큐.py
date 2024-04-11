06fae6d06ae11d0433db8442e349d02320bcf264
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-04-12 02:44:03 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 5148 ms, Memory: 178524 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,43 @@
+import sys
+import heapq
+
+T = int(input())
+for _ in range(T):
+    k = int(input())
+    min_heap = []
+    max_heap = []
+    nums = {}
+    for i in range(k):
+        op, x = sys.stdin.readline().split()
+        x = int(x)
+        if op == 'I':
+            if x in nums:
+                nums[x] += 1
+            else:
+                nums[x] = 1
+                heapq.heappush(min_heap, x)
+                heapq.heappush(max_heap, -x)
+        elif op == 'D':
+            if min_heap and max_heap and nums:
+                if x == 1:
+                    while -max_heap[0] not in nums or nums[-max_heap[0]] == 0:
+                        heapq.heappop(max_heap) * -1
+                    nums[-max_heap[0]] -= 1
+                    if nums[-max_heap[0]] == 0:
+                        del (nums[-max_heap[0]])
+
+                else:
+                    while min_heap[0] not in nums or nums[min_heap[0]] == 0:
+                        heapq.heappop(min_heap)
+                    nums[min_heap[0]] -= 1
+                    if nums[min_heap[0]] == 0:
+                        del (nums[min_heap[0]])
+
+    if nums:
+        while min_heap[0] not in nums:
+            heapq.heappop(min_heap)
+        while -max_heap[0] not in nums:
+            heapq.heappop(max_heap)
+        print(-max_heap[0], min_heap[0])
+    else:
+        print('EMPTY')

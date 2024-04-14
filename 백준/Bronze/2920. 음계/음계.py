3ae2a7bc729adfe6076487c75c5530639ca081f0
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-04-14 23:55:44 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,9 +1,7 @@
-asc = '1 2 3 4 5 6 7 8'
-desc = '8 7 6 5 4 3 2 1'
 nums = input()
-if asc == nums:
+if nums == '1 2 3 4 5 6 7 8':
     print('ascending')
-elif desc == nums:
+elif nums == '8 7 6 5 4 3 2 1':
     print('descending')
 else:
     print('mixed')

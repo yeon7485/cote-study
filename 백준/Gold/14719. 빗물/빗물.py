1805d94fb1932457928e4fbe6683c3a37630b3e1
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-04-22 00:41:19 +0900

    [Gold V] Title: 빗물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -3,9 +3,14 @@ height = list(map(int, input().split()))
 ans = 0
 
 for i in range(1, W - 1):
+    # 인덱스 기준 왼쪽/오른쪽에서 제일 높은 블록 구하기
     max_left = max(height[:i])
     max_right = max(height[i:])
-    rain = min(max_left, max_right)
-    ans += max(0, rain - height[i])
+
+    # 빗물이 쌓일 수 있는 높이
+    rain_height = min(max_left, max_right)
+    # 고이는 빗물 더해주기
+    if rain_height > height[i]:
+        ans += rain_height - height[i]
 
 print(ans)

98b2f77500b433146353f24bf2d0fb5dbbff11b8
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-11-15 18:36:37 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+S = 0
+for i in range(N):
+    S += A[i] * B[i]
+
+print(S)

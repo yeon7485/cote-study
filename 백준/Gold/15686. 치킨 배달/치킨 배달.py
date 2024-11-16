4ca07d373235d853cc7e5f0515ddd0895b2df077
박세연 <55083958+yeon7485@users.noreply.github.com>
2024-11-17 01:00:00 +0900

    [Gold V] Title: 치킨 배달, Time: 348 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,38 +1,39 @@
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 chicken = []
-home = []
+house = []
+
 for i in range(N):
     for j in range(N):
-        if board[i][j] == 2:
+        if board[i][j] == 1:
+            house.append([i, j])
+        elif board[i][j] == 2:
             chicken.append([i, j])
-        elif board[i][j] == 1:
-            home.append([i, j])
 
-visited = [False] * len(chicken)
-temp = []
-result = int(1e9)
+visited_chicken = [False] * len(chicken)
+sel_chicken = []
+result = 1e10
 
 
-def func(n, s):
+def func(n, count):
     global result
-    if n == M:
-        ans = 0
-        for h in home:
-            min_home = 2 * N
-            for t in temp:
-                min_home = min(min_home, abs(t[0] - h[0]) + abs(t[1] - h[1]))
-            ans += min_home
-        result = min(result, ans)
+    if count == M:
+        sum_house = 0
+        for h in house:
+            min_house = 1e10
+            for sel in sel_chicken:
+                min_house = min(abs(h[0] - chicken[sel][0]) + abs(h[1] - chicken[sel][1]), min_house)
+            sum_house += min_house
+        result = min(sum_house, result)
         return
 
-    for i in range(s, len(chicken)):
-        if not visited[i]:
-            temp.append(chicken[i])
-            visited[i] = True
-            func(n + 1, i)
-            temp.pop()
-            visited[i] = False
+    for i in range(n, len(chicken)):
+        if not visited_chicken[i]:
+            sel_chicken.append(i)
+            visited_chicken[i] = True
+            func(i + 1, count + 1)
+            visited_chicken[i] = False
+            sel_chicken.pop()
 
 
 func(0, 0)

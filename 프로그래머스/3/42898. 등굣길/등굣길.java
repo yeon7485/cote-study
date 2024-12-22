fd879776608edc32ad7dd127d4d88c48e1b9194a
seiyeon <55083958+yeon7485@users.noreply.github.com>
2024-12-22 22:11:45 +0900

    [level 3] Title: 등굣길, Time: 1.14 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n][m];
+        dp[0][0] = 1;
+            
+        for(int[] pud : puddles) {
+            dp[pud[1] - 1][pud[0] - 1] = -1;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(dp[i][j] == -1) {
+                    continue;
+                }
+                
+                if(i != 0 && dp[i - 1][j] != -1) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if(j != 0 && dp[i][j - 1] != -1) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+                dp[i][j] %= 1000000007;
+            }
+        }
+        
+        return dp[n-1][m-1];
+    }
+}
